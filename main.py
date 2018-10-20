f680d71ba3a43768fb60e9c2249c8858cfc43186
Victor Lee <victorlee0911@gmail.com>
2018-10-20 13:39:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ reddit = praw.Reddit(client_id='jW52Y1LgB9macg',
 print(reddit.read_only)
 all = ""
 for submission in reddit.subreddit('wallstreetbets').top(limit=100):
+    
+    # submission.comments.replace_more(limit=None)
+    # for comment in submission.comments.list():
+    #     if(comment.author!="WSBVoteBot" and comment.author!="auto_moderator"):
+    #         all+= comment.body
     submission.comments.replace_more(limit=None)
-    for comment in submission.comments.list():
-        if(comment.author!="WSBVoteBot" and comment.author!="auto_moderator"):
-            all+= comment.body
+    for top_level_comment in submission.comments:
+        all+=top_level_comment.body+" "
+        for second_level_comment in top_level_comment.replies:
+            all+=top_level_comment.body+ " "
+            for third_level_comment in second_level_comment.replies:
+                all+=top_level_comment.body+ " "
 tokenized = word_tokenize(all)
 print(tokenized.length)
