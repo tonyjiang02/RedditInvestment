c03e56ace6f1e46ad4c6620472db41da2b408437
Victor Lee <victorlee0911@gmail.com>
2018-10-20 15:27:37 -0700

    database

---
+++ database.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('hshacks-investment-a60cb5d9bfb0.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
