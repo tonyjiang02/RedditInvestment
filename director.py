64094f6a00ab8b440905382ba76f2afc6bb42131
Tony Jiang <tonyjiang02@gmail.com>
2018-10-20 14:41:47 -0700

    f

--- director.py
+++ director.py
@@ -2,10 +2,17 @@ import main
 import stocks
 import nltk
 from nltk import word_tokenize 
+import operator
 symbols = stocks.symbols
 names = stocks.names
-curr = main.readSub("stocks","hot",10)
+curr = main.readSub("wallstreetbets","top",10)
 tokenize = word_tokenize(curr)
+dictionary = {}
 for stuff in tokenize:
     if(stuff in symbols and len(stuff)>1):
-        print(stuff)
+        if(stuff in dictionary):
+            dictionary[stuff]+=1
+        else:
+            dictionary[stuff]=1
+sortedDictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse = True)
+print(sortedDictionary)
