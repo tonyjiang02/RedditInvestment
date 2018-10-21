9a08449021b5e783b984da41bde97fb101539a0c
Tony Jiang <tonyjiang02@gmail.com>
2018-10-21 04:22:23 -0700

    database and trader

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import director
 from ticker import Ticker
-dictionary = director.dictionarySort
 cred = credentials.Certificate('hshacks-investment-a60cb5d9bfb0.json')
 firebase_admin.initialize_app(cred)
 
@@ -36,13 +34,46 @@ def getData(stock, date1, date2):
     return [dict1, dict2]
 def getPortfolio():
     doc_ref = db.collection(u'portfolio').document(u'portfolio1')
+    history_ref = doc_ref.collection(u'history').get()
+    portfolio_ref = doc_ref.collection(u'portfolio').get()
+    history_arr = []
+    portfolio_arr = []
+    for doc in history_ref:
+        print("one")
+        history_arr.append(doc)
+    for doc in portfolio_ref:
+        portfolio_arr.append(doc)
     doc = doc_ref.get()
-    dict1 = doc.to_dict()
-    return dict1
-def uploadPortfolio(balance, portfolio, history):
+    superdoc = doc.to_dict()
+    return [superdoc,history_arr,portfolio_arr]
+def uploadPortfolio(stock_object, id):
+    doc_ref = db.collection(u'portfolio').document(u'portfolio1').collection(u'portfolio').document(u''+str(id))
+    doc_ref.set({
+        u'symbol':stock_object.symbol,
+        u'quantity':stock_object.quantity,
+        u'buyprice':stock_object.buyprice,
+        u'buydate':str(stock_object.buydate),
+        u'tradetype':stock_object.tradetype
+    })
+def uploadHistory(sold_stock, id):
+    doc_ref = db.collection(u'portfolio').document(u'portfolio1').collection(u'history').document(u''+str(id))
+    doc_ref.set({
+        u'symbol': sold_stock.symbol,
+        u'quantity': sold_stock.quantity,
+        u'buyprice': sold_stock.buyprice,
+        u'sellprice' :sold_stock.sellprice,
+        u'buydate' : str(sold_stock.buydate),
+        u'selldate' : str(sold_stock.selldate),
+        u'tradetype' : sold_stock.tradetype,
+        u'profit' : sold_stock.profit
+    })
+def updateVars(balance, historyid, portfolioid):
     doc_ref = db.collection(u'portfolio').document(u'portfolio1')
     doc_ref.set({
         u'balance':balance,
-        u'portfolio':portfolio,
-        u'history':history
+        u'historyid':historyid,
+        u'portfolioid':portfolioid
     })
+def deletePortfolio(id):
+    doc_ref = db.collection(u'portfolio').document(u'portfolio1').collection(u'portfolio').document(u''+str(id))
+    doc_ref.delete()
--- trader.py
+++ trader.py
@@ -4,10 +4,15 @@ from stockobject import stockObject
 from stockobject import soldObject
 import datetime
 import database
-dictvalues = database.getPortfolio()
-balance = dictvalues["balance"]
-portfolio = dictvalues["portfolio"]
-history = dictvalues["history"]
+arr = database.getPortfolio()
+balance = arr[0]["balance"]
+historyid = arr[0]["historyid"]
+portfolioid = arr[0]["portfolioid"]
+print(balance)
+print(historyid)
+print(portfolioid)
+portfolio = arr[2]
+history = arr[1]
 ts = TimeSeries(key='FEX2ZCRX0XGU1ZL7', output_format='pandas')
 def getPrice(ticket):
     global ts
@@ -16,30 +21,37 @@ def getPrice(ticket):
 def buy(symbol, quantity):
     global balance
     price = getPrice(symbol)
-    stock_object = stockObject(symbol,quantity,price,datetime.datetime,"buy")
-    portfolio.append(stock_object)
+    stock_object = stockObject(symbol,quantity,price,datetime.date,"buy")
+    database.uploadPortfolio(stock_object,portfolioid)
     balance -= quantity*price
+    database.updateVars(balance,historyid,portfolioid+1)
 # def short(symbol,quantity):
 #     global balance
 #     price = getPrice(symbol)
 #     stock_object = stockObject(symbol,quantity,price,datetime.datetime,"short")
 #     portfolio.append(stock_object)
 #     balance-=quantity*price
-def sell(index, quantity):
-    stock_object = portfolio[index]
-    stock_sold = None
-    price = getPrice(stock_object.symbol)
-    if(quantity <=stock_object.quantity):
-        portfolio[index].quantity-=quantity
-        profit = calculateProfit(price,stock_object,quantity)
-        balance+=profit
-        stock_sold = soldObject(self,stock_object.symbol,quantity,stock_object.buyprice,price,stock_object.buydate,datetime.datetime,stock_object.tradetype,profit)
-        history.append(stock_sold)
+def sell(id):
+    global balance
+    global portfolio
+    stock_object = {}
+    for doc in portfolio:
+        if(doc.id == str(id)):
+            print("stock")
+            stock_object = doc.to_dict()
+    price = getPrice(stock_object["symbol"])
+    profit = calculateProfit(price,stock_object)
+    balance+=profit
+    stock_sold = soldObject(stock_object["symbol"],stock_object["quantity"],stock_object["buyprice"],price,stock_object["buydate"],datetime.date,stock_object["tradetype"],profit)
+    database.deletePortfolio(id)
+    database.uploadHistory(stock_sold,historyid)
+    database.updateVars(balance,historyid+1,portfolioid)
 
-def calculateProfit(currPrice, stock_object, quantity):
-    if(tradeType=="buy"):
-        return price*quantity - stock_object.buyprice*quantity
+def calculateProfit(currPrice, stock_object):
+    if(stock_object["tradetype"]=="buy"):
+        return currPrice*stock_object["quantity"] - stock_object["buyprice"]*stock_object["quantity"]
     # if(tradeType=="short"):
     #     return stock_object.buyprice*quantity-price*quantity
-buy("AAPL", 100)
-print(balance)
+
+buy("GOOG",12)
+sell(2)
